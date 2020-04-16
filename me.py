707f9a185318bfc8cffce415827c72b00ae8224b
Kate Kulinski <35544223+katurian@users.noreply.github.com>
2020-04-16 02:51:47 -0400

    Update me.py

--- me.py
+++ me.py
@@ -51,3 +51,105 @@ def editBio(text):
     
     r = requests.patch(f'https://api.letterboxd.com/api/v0/me?apikey={apikey}&nonce={nonce}&timestamp={timestamp}&signature={signature}', headers={'Authorization': bearertoken, 'Content-Type': 'application/json', 'Accept': 'application/json'}, data=body, verify=False)
    
+def editFamilyName(text):
+    nonce = uuid.uuid4()
+    timestamp = int(time.time())
+    method = "PATCH"
+    url = f'https://api.letterboxd.com/api/v0/me?apikey={apikey}&nonce={nonce}&timestamp={timestamp}'
+    body = {"familyName": text}
+    body = json.dumps(body)
+    bytestring = b"\x00".join(
+        [str.encode(method), str.encode(url), str.encode(body)]
+    )
+    signature = hmac.new(
+        str.encode(apisecret), bytestring, digestmod=hashlib.sha256
+    )
+    signature = signature.hexdigest()
+    
+    r = requests.patch(f'https://api.letterboxd.com/api/v0/me?apikey={apikey}&nonce={nonce}&timestamp={timestamp}&signature={signature}', headers={'Authorization': bearertoken, 'Content-Type': 'application/json', 'Accept': 'application/json'}, data=body, verify=False)
+    
+def editPronoun(code):
+    nonce = uuid.uuid4()
+    timestamp = int(time.time())
+    method = "PATCH"
+    url = f'https://api.letterboxd.com/api/v0/me?apikey={apikey}&nonce={nonce}&timestamp={timestamp}'
+    body = {"pronoun": code}
+    body = json.dumps(body)
+    bytestring = b"\x00".join(
+        [str.encode(method), str.encode(url), str.encode(body)]
+    )
+    signature = hmac.new(
+        str.encode(apisecret), bytestring, digestmod=hashlib.sha256
+    )
+    signature = signature.hexdigest()
+    
+    r = requests.patch(f'https://api.letterboxd.com/api/v0/me?apikey={apikey}&nonce={nonce}&timestamp={timestamp}&signature={signature}', headers={'Authorization': bearertoken, 'Content-Type': 'application/json', 'Accept': 'application/json'}, data=body, verify=False)
+    
+def editName(text):
+    nonce = uuid.uuid4()
+    timestamp = int(time.time())
+    method = "PATCH"
+    url = f'https://api.letterboxd.com/api/v0/me?apikey={apikey}&nonce={nonce}&timestamp={timestamp}'
+    body = {"givenName": text}
+    body = json.dumps(body)
+    bytestring = b"\x00".join(
+        [str.encode(method), str.encode(url), str.encode(body)]
+    )
+    signature = hmac.new(
+        str.encode(apisecret), bytestring, digestmod=hashlib.sha256
+    )
+    signature = signature.hexdigest()
+    
+    r = requests.patch(f'https://api.letterboxd.com/api/v0/me?apikey={apikey}&nonce={nonce}&timestamp={timestamp}&signature={signature}', headers={'Authorization': bearertoken, 'Content-Type': 'application/json', 'Accept': 'application/json'}, data=body, verify=False)
+
+def editEmail(text):
+    nonce = uuid.uuid4()
+    timestamp = int(time.time())
+    method = "PATCH"
+    url = f'https://api.letterboxd.com/api/v0/me?apikey={apikey}&nonce={nonce}&timestamp={timestamp}'
+    body = {"emailAddress": text}
+    body = json.dumps(body)
+    bytestring = b"\x00".join(
+        [str.encode(method), str.encode(url), str.encode(body)]
+    )
+    signature = hmac.new(
+        str.encode(apisecret), bytestring, digestmod=hashlib.sha256
+    )
+    signature = signature.hexdigest()
+    
+    r = requests.patch(f'https://api.letterboxd.com/api/v0/me?apikey={apikey}&nonce={nonce}&timestamp={timestamp}&signature={signature}', headers={'Authorization': bearertoken, 'Content-Type': 'application/json', 'Accept': 'application/json'}, data=body, verify=False)
+    
+def editLocation(text):
+    nonce = uuid.uuid4()
+    timestamp = int(time.time())
+    method = "PATCH"
+    url = f'https://api.letterboxd.com/api/v0/me?apikey={apikey}&nonce={nonce}&timestamp={timestamp}'
+    body = {"location": text}
+    body = json.dumps(body)
+    bytestring = b"\x00".join(
+        [str.encode(method), str.encode(url), str.encode(body)]
+    )
+    signature = hmac.new(
+        str.encode(apisecret), bytestring, digestmod=hashlib.sha256
+    )
+    signature = signature.hexdigest()
+    
+    r = requests.patch(f'https://api.letterboxd.com/api/v0/me?apikey={apikey}&nonce={nonce}&timestamp={timestamp}&signature={signature}', headers={'Authorization': bearertoken, 'Content-Type': 'application/json', 'Accept': 'application/json'}, data=body, verify=False)
+        
+ def editWebsite(text):
+    nonce = uuid.uuid4()
+    timestamp = int(time.time())
+    method = "PATCH"
+    url = f'https://api.letterboxd.com/api/v0/me?apikey={apikey}&nonce={nonce}&timestamp={timestamp}'
+    body = {"website": text}
+    body = json.dumps(body)
+    bytestring = b"\x00".join(
+        [str.encode(method), str.encode(url), str.encode(body)]
+    )
+    signature = hmac.new(
+        str.encode(apisecret), bytestring, digestmod=hashlib.sha256
+    )
+    signature = signature.hexdigest()
+    
+    r = requests.patch(f'https://api.letterboxd.com/api/v0/me?apikey={apikey}&nonce={nonce}&timestamp={timestamp}&signature={signature}', headers={'Authorization': bearertoken, 'Content-Type': 'application/json', 'Accept': 'application/json'}, data=body, verify=False)
+    
