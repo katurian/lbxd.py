a1a889fde1e900fd5da50248e2ed5fe45b804f17
Kate Kulinski <35544223+katurian@users.noreply.github.com>
2020-04-16 02:35:39 -0400

    Update me.py

--- me.py
+++ me.py
@@ -16,4 +16,22 @@ def getMe():
     r = requests.get(f'https://api.letterboxd.com/api/v0/me?apikey={apikey}&nonce={nonce}&timestamp={timestamp}&signature={signature}', headers={'Authorization': bearertoken}, verify=False)
     resultJSON = r.json()
     return resultJSON
+
+def editBio(text):
+    nonce = uuid.uuid4()
+    timestamp = int(time.time())
+    method = "PATCH"
+    url = f'https://api.letterboxd.com/api/v0/me?apikey={apikey}&nonce={nonce}&timestamp={timestamp}'
+    body = {"bio": text}
+    body = json.dumps(body)
+    bytestring = b"\x00".join(
+        [str.encode(method), str.encode(url), str.encode(body)]
+    )
+    signature = hmac.new(
+        str.encode(apisecret), bytestring, digestmod=hashlib.sha256
+    )
+    signature = signature.hexdigest()
+    
+    r = requests.patch(f'https://api.letterboxd.com/api/v0/me?apikey={apikey}&nonce={nonce}&timestamp={timestamp}&signature={signature}', headers={'Authorization': bearertoken, 'Content-Type': 'application/json', 'Accept': 'application/json'}, data=body, verify=False)
+    
     
