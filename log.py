3a998ba6e1de531268de7fc0192a774ea583f05e
Kate Kulinski <35544223+katurian@users.noreply.github.com>
2020-04-20 00:18:33 -0400

    Update log.py

--- log.py
+++ log.py
@@ -63,3 +63,20 @@ def getEntryStats(code):
     r = requests.get(f'https://api.letterboxd.com/api/v0/log-entry/{code}/statistics?apikey={apikey}&nonce={nonce}&timestamp={timestamp}&signature={signature}', verify=False)
     resultJSON = r.json()
     return resultJSON
+
+def logFilm(code, review, rating, spoilers, rewatched, date, liked, tags):
+    nonce = uuid.uuid4()
+    timestamp = int(time.time())
+    method = "POST"
+    url = f'https://api.letterboxd.com/api/v0/log-entries?apikey={apikey}&nonce={nonce}&timestamp={timestamp}'
+    body = {'rating': rating, 'review':{'containsSpoilers': spoilers, 'text': review}, 'diaryDetails':{'rewatch': rewatched, 'diaryDate': date}, 'like': liked, 'filmId': code, 'tags':tags}
+    body = json.dumps(body)
+    headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': bearertoken}
+    bytestring = b"\x00".join(
+        [str.encode(method), str.encode(url), str.encode(body)]
+    )
+    signature = hmac.new(
+        str.encode(apisecret), bytestring, digestmod=hashlib.sha256
+    )
+    signature = signature.hexdigest()
+    r = requests.post(f'https://api.letterboxd.com/api/v0/log-entries?apikey={apikey}&nonce={nonce}&timestamp={timestamp}&signature={signature}', headers=headers, data=body, verify=False)
