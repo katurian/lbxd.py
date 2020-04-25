c4437ae873e0f223d0b2fbd00b12ab1a26a8434a
Kate Kulinski <35544223+katurian@users.noreply.github.com>
2020-04-25 19:16:03 -0400

    Update list.py

--- list.py
+++ list.py
@@ -79,3 +79,30 @@ def getListStats(code):
     r = requests.get(f'https://api.letterboxd.com/api/v0/list/{code}/statistics?apikey={apikey}&nonce={nonce}&timestamp={timestamp}&signature={signature}', verify=False)
     resultJSON = r.json()
     return resultJSON
+
+def createList(name, description, entries, tags, ranked):
+    entrieslist = []
+    count = 1
+    for entry in entries:
+        entriesdict = {
+            "rank":    count,
+            "containsSpoilers": False,
+            "film":  entry,
+        }
+        count = count + 1
+        entrieslist.append(entriesdict)
+    nonce = uuid.uuid4()
+    timestamp = int(time.time())
+    method = "POST"
+    url = f'https://api.letterboxd.com/api/v0/lists?apikey={apikey}&nonce={nonce}&timestamp={timestamp}'
+    body = {"published":True,"tags":tags,"name":name,"entries":entrieslist,"ranked":ranked,"description":description}
+    body = json.dumps(body)
+    headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': bearertoken}
+    bytestring = b"\x00".join(
+        [str.encode(method), str.encode(url), str.encode(body)]
+    )
+    signature = hmac.new(
+        str.encode(apisecret), bytestring, digestmod=hashlib.sha256
+    )
+    signature = signature.hexdigest()
+    r = requests.post(f'https://api.letterboxd.com/api/v0/lists?apikey={apikey}&nonce={nonce}&timestamp={timestamp}&signature={signature}', headers=headers, data=body, verify=False)
