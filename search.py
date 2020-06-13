8572cdda89c114de8d5fcc01240b09373f6aaf8b
Kate Kulinski <35544223+katurian@users.noreply.github.com>
2020-06-13 14:05:12 -0400

    Update search.py

--- search.py
+++ search.py
@@ -53,3 +53,21 @@ def searchLists(query):
     r = requests.get(f'https://api.letterboxd.com/api/v0/search?include=ListSearchItem&perPage=10&searchMethod=FullText&input={query}&apikey={apikey}&nonce={nonce}&timestamp={timestamp}&signature={signature}', verify=False)
     resultJSON = r.json()
     return resultJSON
+
+def searchTags(query):
+    query = query.replace(" ", "%20")
+    nonce = uuid.uuid4()
+    timestamp = int(time.time())
+    method = "GET"
+    url = f'https://api.letterboxd.com/api/v0/search?include=TagSearchItem&perPage=10&searchMethod=FullText&input={query}&apikey={apikey}&nonce={nonce}&timestamp={timestamp}'
+    bytestring = b"\x00".join(
+        [str.encode(method), str.encode(url), str.encode("")]
+    )
+    signature = hmac.new(
+        str.encode(apisecret), bytestring, digestmod=hashlib.sha256
+    )
+    signature = signature.hexdigest()
+    
+    r = requests.get(f'https://api.letterboxd.com/api/v0/search?include=TagSearchItem&perPage=10&searchMethod=FullText&input={query}&apikey={apikey}&nonce={nonce}&timestamp={timestamp}&signature={signature}', verify=False)
+    resultJSON = r.json()
+    return resultJSON
