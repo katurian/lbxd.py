5ab0e9b73fbefad74a39de6a3f8a030914bfeb07
Kate Kulinski <35544223+katurian@users.noreply.github.com>
2020-06-13 15:02:02 -0400

    Update log.py

--- log.py
+++ log.py
@@ -79,5 +79,22 @@ def logFilm(code, review, rating, spoilers, rewatched, date, liked, tags):
     )
     signature = signature.hexdigest()
     r = requests.post(f'https://api.letterboxd.com/api/v0/log-entries?apikey={apikey}&nonce={nonce}&timestamp={timestamp}&signature={signature}', headers={'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': bearertoken}, data=body, verify=False)
+     
+def getReviewsByTag(tagger, query):
+    query = query.replace(" ", "%20")
+    nonce = uuid.uuid4()
+    timestamp = int(time.time())
+    method = "GET"
+    url = f'https://api.letterboxd.com/api/v0/log-entries?tagger={tagger}&sort=WhenAdded&where=HasReview&includeTaggerFriends=None&tagCode={query}&perPage=300&apikey={apikey}&nonce={nonce}&timestamp={timestamp}'
+    bytestring = b"\x00".join(
+        [str.encode(method), str.encode(url), str.encode("")]
+    )
+    signature = hmac.new(
+        str.encode(apisecret), bytestring, digestmod=hashlib.sha256
+    )
+    signature = signature.hexdigest()
     
+    r = requests.get(f'https://api.letterboxd.com/api/v0/log-entries?tagger={tagger}&sort=WhenAdded&where=HasReview&includeTaggerFriends=None&tagCode={query}&perPage=300&apikey={apikey}&nonce={nonce}&timestamp={timestamp}&signature={signature}', verify=False)
+    resultJSON = r.json()
+    return resultJSON
     
